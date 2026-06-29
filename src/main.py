79ef56bf16b333e7a5b3a6e1e1cf1d62ef028c87
inesmassa <inesurvivor@gmail.com>
2026-06-29 15:45:32 +0200

    questions

---
+++ src/main.py
@@ -0,0 +1,43 @@
+from dataset import OpenDataset
+from model import Brain_Model
+from train import train
+from evaluate import evaluate
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+
+def main():
+
+    train_dataset = OpenDataset()
+    test_dataset = OpenDataset()
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=16,
+        shuffle=True
+    )
+
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=16,
+        shuffle=False
+    )
+
+    model = Brain_Model()
+
+    loss_fn = nn.BCEWithLogitsLoss()
+
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=1e-3
+    )
+
+    train(model, train_loader, optimizer, loss_fn)
+
+    evaluate(model, test_loader, loss_fn)
+
+
+if __name__ == "__main__":
+    main()
--- src/train.py
+++ src/train.py
@@ -1,14 +1,5 @@
 # entrainement
 from dataset import OpenDataset
-from torch.utils.data import DataLoader
 
 def train(model, dataloader, optimizer, loss_fn):
     pass
-
-train_dataset = OpenDataset()
-
-train_loader = DataLoader(
-    train_dataset,
-    batch_size=16,
-    shuffle=True
-)
