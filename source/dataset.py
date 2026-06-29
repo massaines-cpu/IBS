299996827bd87cbd7e4c90c5e2c2d3caed4c9153
inesmassa <inesurvivor@gmail.com>
2026-06-29 13:51:21 +0200

    debut dataset.py

--- source/dataset.py
+++ source/dataset.py
@@ -1,1 +1,30 @@
 # dataset pytorch
+
+from torch.utils.data import Dataset
+
+class Open_dataset(Dataset):
+
+    def __init__(self, data, transform=None):
+        self.data = data
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # recup les chemins des 2 eeg
+        # eeg_A_path =
+        # eeg_B_path =
+
+        # charger les eeg
+        # eeg_A = load_eeg(eeg_A_path)
+        # eeg_B = load_eeg(eeg_B_path)
+
+        # appliquer prétraitements
+
+        # recup label
+        # label =
+
+        # return données
+        # return eeg_A, eeg_B, label
+        pass
